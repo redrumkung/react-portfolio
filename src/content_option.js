89ae1c5c672939d9325b38907e1f2dc1986cd73a
redrumkumg <n.ronnapat@github.com>
2024-07-10 13:07:59 +0700

    Ronnapat edit

--- src/content_option.js
+++ src/content_option.js
@@ -1,166 +1,192 @@
-const logotext = "JOHN";
+const logotext = "Ronnapat Narongdej";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+  title: "Ronnapat Narongdej",
+  description: "I’m John Ronnapat Narongdej Full stack developer",
 };
 
 const introdata = {
-    title: "I’m John Doe",
-    animated: {
-        first: "I love coding",
-        second: "I code cool websites",
-        third: "I develop mobile apps",
-    },
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
-    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+  title: "I’m Ronnapat Narongdej",
+  animated: {
+    first: "I love coding",
+    second: "I code cool websites",
+    third: "I develop apps",
+  },
+  description:
+    "A graduate of Generation’s Junior Software Developer BootCamp and Software Park’s CodeCamp, seeking Full-Stack developer or Business Analyst position.",
+  your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
 };
 
 const dataabout = {
-    title: "abit about my self",
-    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+  title: "abit about my self",
+  aboutme:
+    "I am a graduate of Generation’s Junior Software Developer Bootcamp and Software Park’s Full-Stack Developer CodeCamp, seeking a Full-Stack developer position. Previously, I worked in other fields, but now I have a passion for Full-Stack developer. This career change has equipped me with new skills and diverse perspectives, inspiring me to work happily and efficiently.",
 };
-const worktimeline = [{
-        jobtitle: "Designer of week",
-        where: "YAdfi",
-        date: "2020",
-    },
-    {
-        jobtitle: "Designer of week",
-        where: "Jamalya",
-        date: "2019",
-    },
-    {
-        jobtitle: "Designer of week",
-        where: "ALquds",
-        date: "2019",
-    },
+const worktimeline = [
+  {
+    jobtitle: "Junior Software Developer Bootcamp",
+    where: "Generation Thailand",
+    date: "April 2024 - July 2024",
+  },
+  {
+    jobtitle: "Full Stack Developer CodeCamp",
+    where: "Software Park",
+    date: "November 2023 - March 2024",
+  },
+  {
+    jobtitle: "Social Media Content QC",
+    where: "Transcosmos Thailand",
+    date: "August 2022 - May 2023",
+  },
 ];
 
-const skills = [{
-        name: "Python",
-        value: 90,
-    },
-    {
-        name: "Djano",
-        value: 85,
-    },
-    {
-        name: "Javascript",
-        value: 80,
-    },
-    {
-        name: "React",
-        value: 60,
-    },
-    {
-        name: "Jquery",
-        value: 85,
-    },
+const skills = [
+  {
+    name: "HTML",
+    value: 85,
+  },
+  {
+    name: "CSS",
+    value: 70,
+  },
+  {
+    name: "Javascript",
+    value: 80,
+  },
+  {
+    name: "React",
+    value: 90,
+  },
+  {
+    name: "Express",
+    value: 75,
+  },
+  {
+    name: "NodeJS",
+    value: 75,
+  },
 ];
 
-const services = [{
-        title: "UI & UX Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
-    {
-        title: "Mobile Apps",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
-    {
-        title: "Wordpress Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
+const services = [
+  {
+    title: "UI & UX Design",
+    description:
+      "Capable of designing UX/UI using Figma.",
+  },
+  {
+    title: "Full Stack Development",
+    description:
+      "HTML, CSS, and JavaScript ; Front End development using React, Back End development using Express, and database management with MySQL and MongoDB.",
+  },
+  {
+    title: "Business Analyst",
+    description:
+      "Gather customer requirements and communicate them effectively with the development team. Possess strong communication skills, excellent negotiation abilities, high patience, and exceptional intuition.",
+  },
 ];
 
-const dataportfolio = [{
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/800/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
+const dataportfolio = [
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "JSD7 - E-commerce Project Front-end",
+    link: "https://github.com/JSD7-CMFM/JSD7-CMFM-Frontend",
+  },
+  {
+    img: "https://picsum.photos/400/900/?grayscale",
+    description:
+      "JSD7 - E-commerce Project Back-end",
+    link: "https://github.com/JSD7-CMFM/JSD7-CMFM-Backend",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description:
+      "WatchesPlus+ Front-end",
+    link: "https://github.com/ohmmpakpoomm/watchesPlus_frontend",
+  },
+  {
+    img: "https://picsum.photos/400/1000/?grayscale",
+    description:
+      "WatchesPlus+ Back-end",
+    link: "https://github.com/ohmmpakpoomm/watchesPlus_backend",
+  },
+  {
+    img: "https://picsum.photos/400/800/?grayscale",
+    description:
+      "Your Trip Front-end",
+    link: "https://github.com/redrumkung/CC16_PersonalProject_web",
+  },
+  {
+    img: "https://picsum.photos/400/1100/?grayscale",
+    description:
+      "Your Trip Back-end",
+    link: "https://github.com/redrumkung/CC16_PersonalProject_api",
+  },
 
-    {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/550/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
+//   {
+//     img: "https://picsum.photos/400/600/?grayscale",
+//     description:
+//       "The wisdom of life consists in the elimination of non-essentials.",
+//     link: "#",
+//   },
+//   {
+//     img: "https://picsum.photos/400/300/?grayscale",
+//     description:
+//       "The wisdom of life consists in the elimination of non-essentials.",
+//     link: "#",
+//   },
+//   {
+//     img: "https://picsum.photos/400/?grayscale",
+//     description:
+//       "The wisdom of life consists in the elimination of non-essentials.",
+//     link: "#",
+//   },
+//   {
+//     img: "https://picsum.photos/400/550/?grayscale",
+//     description:
+//       "The wisdom of life consists in the elimination of non-essentials.",
+//     link: "#",
+//   },
+//   {
+//     img: "https://picsum.photos/400/?grayscale",
+//     description:
+//       "The wisdom of life consists in the elimination of non-essentials.",
+//     link: "#",
+//   },
+//   {
+//     img: "https://picsum.photos/400/700/?grayscale",
+//     description:
+//       "The wisdom of life consists in the elimination of non-essentials.",
+//     link: "#",
+//   },
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "name@domain.com",
-    YOUR_FONE: "(555)123-4567",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
-    // creat an emailjs.com account 
-    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
-    YOUR_SERVICE_ID: "service_id",
-    YOUR_TEMPLATE_ID: "template_id",
-    YOUR_USER_ID: "user_id",
+  YOUR_EMAIL: "n.ronnapat@gmail.com",
+  YOUR_FONE: "(+66)6-2324-7557",
+  description:
+    "Feel free to contact me via email or phone. I am available from 08:00 AM to 06:00 PM. ",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
 };
 
 const socialprofils = {
-    github: "https://github.com",
-    facebook: "https://facebook.com",
-    linkedin: "https://linkedin.com",
-    twitter: "https://twitter.com",
+  github: "https://github.com/redrumkung",
+  facebook: "https://www.facebook.com/redrumkung",
+  linkedin: "https://www.linkedin.com/in/ronnapat-narongdej/.com",
+  twitter: "https://twitter.com",
 };
 export {
-    meta,
-    dataabout,
-    dataportfolio,
-    worktimeline,
-    skills,
-    services,
-    introdata,
-    contactConfig,
-    socialprofils,
-    logotext,
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
 };
